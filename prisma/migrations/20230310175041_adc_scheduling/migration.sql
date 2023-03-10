c6bdfc94328d60a212d41a9aa4e1a0f1c182f362
DevLuis <luispavavao@gmail.com>
2023-03-10 15:51:03 -0300

    commit

---
+++ prisma/migrations/20230310175041_adc_scheduling/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "schedulings" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "observations" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" TEXT NOT NULL,
+    CONSTRAINT "schedulings_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE INDEX "schedulings_user_id_idx" ON "schedulings"("user_id");
+
+-- CreateIndex
+CREATE INDEX "accounts_user_id_idx" ON "accounts"("user_id");
+
+-- CreateIndex
+CREATE INDEX "sessions_user_id_idx" ON "sessions"("user_id");
